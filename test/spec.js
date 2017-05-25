2c5eebb366054c4788924ac19058b629dc858880
Alejandro Perdomo <aperdomobo@gmail.com>
2017-05-24 20:45:54 -0500

    improve the first test

--- test/spec.js
+++
@@ -1,7 +0,0 @@
-describe('This is the first example of protractor', () => {
-  it('should have a title', () => {
-    browser.ignoreSynchronization = true;
-    browser.get('http://www.google.com');
-    expect(browser.getTitle()).toEqual('Google');
-  });
-});
