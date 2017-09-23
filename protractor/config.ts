2c690c5a4af83d0bf90dbaadf6c1dbcaa00bdde7
Alejandro Perdomo <aperdomobo@gmail.com>
2017-09-23 14:31:11 -0500

    add consola report

--- protractor/config.ts
+++ protractor/config.ts
@@ -1,11 +1,14 @@
 import { browser, Config } from 'protractor';
+import { reporter }   from './helpers/reporter';
 
 export let config: Config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['../test/Google.spec.js'],
+  helpers: [ './test/helpers/reporter.js'],
   noGlobals: true,
   onPrepare: () => {
+    reporter();
     browser.ignoreSynchronization = true;
   }
 }
