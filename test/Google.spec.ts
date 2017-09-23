6b2fba5530a8410bddb911452d6471729b8c0602
Alejandro Perdomo <aperdomobo@gmail.com>
2017-09-23 16:12:09 -0500

    fix async await file

--- test/Google.spec.ts
+++ test/Google.spec.ts
@@ -2,12 +2,12 @@ import { browser } from 'protractor';
 
 describe('Given a SDET learning protractor', () => {
   describe('when open Google Page', () => {
-    beforeEach(() => {
-      browser.get('http://www.google.com');
+    beforeEach(async () => {
+      await browser.get('http://www.google.com');
     });
       
-    it('then should have a title', () => {
-      expect(browser.getTitle()).toEqual('Google');
+    it('then should have a title', async () => {
+      await expect(browser.getTitle()).toEqual('Google');
     });
   });
 });
