be5077e86a6b2c763e12ccd4489928cacd2420e4
Alejandro Perdomo <aperdomobo@gmail.com>
2017-09-25 00:35:05 -0500

    refactor Product List Page in order to pass product name how parameter

--- src/page/ProductList.page.ts
+++ src/page/ProductList.page.ts
@@ -1,11 +1,21 @@
-import { $, ElementFinder, promise } from 'protractor';
+import { $$, ElementFinder, ElementArrayFinder, promise } from 'protractor';
 
 export class ProductListPage {
-  private get firstItem(): ElementFinder {
-    return $('.product-container img');
+  private get products(): ElementArrayFinder {
+    return $$('.product-container');
   }
 
-  public selectFirstItem(): promise.Promise<void> {
-    return this.firstItem.click();
+  private findByProduct(productName: string): ElementFinder {
+    return this.products
+      .filter((item: ElementFinder) =>
+        item
+          .$('.product-name')
+          .getText()
+          .then((text: string) => text.includes(productName)))
+      .first();
+  }
+
+  public selectProduct(productName: string): promise.Promise<void> {
+    return this.findByProduct(productName).$('img').click();
   }
 }
