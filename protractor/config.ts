21d9eeec1a3d5f41e7497ec3379e43c03261171e
German Potes <germandavid85@gmail.com>
2017-09-28 21:57:04 -0500

    Add saucelabs configuration and execution

--- protractor/config.ts
+++ protractor/config.ts
@@ -1,13 +1,23 @@
 import { browser, Config } from 'protractor';
 import { reporter } from './helpers/reporter';
 
+let sauceUser;
+let sauceKey;
+if (process.env.SAUCE === 'true') {
+  sauceUser = process.env.SAUCE_USERNAME;
+  sauceKey = process.env.SAUCE_ACCESS_KEY;
+}
+
 export let config: Config = {
+  sauceUser,
+  sauceKey,
   framework: 'jasmine',
   specs: ['../test/**/*.spec.js'],
   SELENIUM_PROMISE_MANAGER: false,
   noGlobals: true,
   getPageTimeout: 30000,
   capabilities: {
+    name: 'UI Workshop',
     browserName: 'chrome',
     chromeOptions: {
       args: ['disable-infobars=true --window-size=800,600'],
@@ -15,7 +25,7 @@ export let config: Config = {
     }
   },
   jasmineNodeOpts: {
-    defaultTimeoutInterval: 120000 
+    defaultTimeoutInterval: 120000
   },
   onPrepare: () => {
     reporter();
