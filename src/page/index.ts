7769f7d807ebc78ff73c963c16ea851370de1c2d
Alejandro Perdomo <aperdomobo@gmail.com>
2017-10-01 12:55:06 -0500

    execute scripts (#47)

--- src/page/index.ts
+++ src/page/index.ts
@@ -10,3 +10,4 @@ export { PaymentStepPage } from './PaymentStep.page';
 export { BankPaymentPage } from './BankPayment.page';
 export { OrderResumePage } from './OrderResume.page';
 export { PersonalInformationPage } from './PersonalInformation.page';
+export { IFramePage } from './IFrame.page';
