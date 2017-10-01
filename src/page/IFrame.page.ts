a05b3118ea630e622c7c9c60770589231b500ffc
Alejandro Perdomo <aperdomobo@gmail.com>
2017-10-01 13:17:55 -0500

    working with iframes

--- src/page/IFrame.page.ts
+++ src/page/IFrame.page.ts
@@ -5,6 +5,10 @@ export class IFramePage {
     return $('#IF1');
   }
 
+  private get documentTitleLabel(): ElementFinder {
+    return $('#main h1');
+  }
+
   public setFormFrameHeight(height: number) : promise.Promise<void> {
     return browser
       .executeScript(`document.querySelector("#IF1").setAttribute('height', ${height});`);
@@ -13,5 +17,17 @@ export class IFramePage {
   public async getHeight(): Promise<number> {
     const height = await this.frame.getAttribute('height');
     return Number(height);
+  }
+
+  public getTitle(): promise.Promise<string> {
+    return this.documentTitleLabel.getText();
+  }
+
+  public switchToFrame(): promise.Promise<void> {
+    return browser.switchTo().frame(this.frame.getWebElement());
+  }
+
+  public switchToMainPage(): promise.Promise<void> {
+    return browser.switchTo().defaultContent();
   }
 }
