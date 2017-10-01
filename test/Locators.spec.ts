3dbac8a25cff7373395c15c9f6f98070bf08de65
Alejandro Perdomo <aperdomobo@gmail.com>
2017-10-01 11:40:15 -0500

    add locators

---
+++ test/Locators.spec.ts
@@ -0,0 +1,34 @@
+import { browser } from 'protractor';
+import { PersonalInformationPage } from '../src/page';
+
+describe('Given a page to practice automation', () => {
+  beforeAll(async () => {
+    await browser.get('http://toolsqa.com/automation-practice-form/');
+  });
+
+  describe('when I am training locators', () => {
+    const personalInformationPage = new PersonalInformationPage();
+
+    beforeAll(async () => {
+      await personalInformationPage.fillForm({
+        firstName: 'Alejandro',
+        lastName: 'Perdomo',
+        sex: 'Male',
+        experience: 7,
+        profession: ['Automation Tester'],
+        tools: ['Selenium Webdriver'],
+        continent: 'South America',
+        commands: [
+          'Browser Commands',
+          'Navigation Commands',
+          'Switch Commands',
+          'Wait Commands',
+          'WebElement Commands']
+      });
+    });
+
+    it('the form should be filled', async () => {
+      expect(await personalInformationPage.getPageTitle()).toBe('Practice Automation Form');
+    });
+  });
+});
