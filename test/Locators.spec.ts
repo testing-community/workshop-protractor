6f2426359d0eefce6d1bc40d4dbe01abe488a08a
Alejandro Perdomo <aperdomobo@gmail.com>
2017-10-01 17:07:24 -0500

    fix describe

--- test/Locators.spec.ts
+++ test/Locators.spec.ts
@@ -1,7 +1,7 @@
 import { browser } from 'protractor';
 import { PersonalInformationPage } from '../src/page';
 
-fdescribe('Given a page to practice automation', () => {
+describe('Given a page to practice automation', () => {
   beforeAll(async () => {
     await browser.get('http://toolsqa.com/automation-practice-form/');
   });
