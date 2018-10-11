f9f879a1f309413a47bb12c87e4b0488a0a6ce71
Alejandro Perdomo <aperdomobo@gmail.com>
2018-10-10 21:57:15 -0500

    add zalenium feature (#89)

---
+++ protractor/zalenium.config.ts
@@ -0,0 +1,39 @@
+import { browser, Config } from 'protractor';
+import { reporter } from './helpers/reporter';
+
+const firefoxConfig = {
+  browserName: 'firefox',
+  platform: 'linux',
+  name: 'firefox-tests',
+  shardTestFiles: true,
+  maxInstances: 1
+};
+
+const chromeConfig = {
+  browserName: 'chrome',
+  name: 'chrome-tests',
+  shardTestFiles: true,
+  maxInstances: 1
+};
+
+const multiCapabilities = [chromeConfig, firefoxConfig];
+
+export let config: Config = {
+  multiCapabilities,
+  framework: 'jasmine',
+  specs: ['../test/**/*.spec.js'],
+  SELENIUM_PROMISE_MANAGER: false,
+  noGlobals: true,
+  getPageTimeout: 30000,
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 120000
+  },
+  onPrepare: () => {
+    reporter();
+    browser.ignoreSynchronization = true;
+    browser.manage().timeouts().implicitlyWait(0);
+  },
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
+  seleniumAddress: 'http://localhost:4444/wd/hub'
+};
